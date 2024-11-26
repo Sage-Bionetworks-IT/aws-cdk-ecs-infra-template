baa820bc22cbc405c7867a1d6c3621fc6cb6c422
Khai Do <3697686+zaro0508@users.noreply.github.com>
2024-11-26 07:54:30 -0800

    initial creation of github template (#2)
    Create a github template to help setup an infrastructure deployment for
    a containerized application to AWS ECS.
    
    These files are from the Sage-Bionetworks-IT/agora-infra-v3 repo (commit e17983ab)[1]
    
    [1] https://github.com/Sage-Bionetworks-IT/agora-infra-v3/tree/e17983abd583d0878a1b2482bbf0ab7cd5bc02c5

---
+++ src/load_balancer_stack.py
@@ -0,0 +1,29 @@
+import aws_cdk as cdk
+
+from aws_cdk import (
+    aws_ec2 as ec2,
+    aws_elasticloadbalancingv2 as elbv2,
+)
+
+from constructs import Construct
+
+
+class LoadBalancerStack(cdk.Stack):
+    """
+    API Gateway to allow access to ECS app from the internet
+    """
+
+    def __init__(
+        self, scope: Construct, construct_id: str, vpc: ec2.Vpc, **kwargs
+    ) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        self.alb = elbv2.ApplicationLoadBalancer(
+            self, "AppLoadBalancer", vpc=vpc, internet_facing=True
+        )
+        cdk.CfnOutput(
+            self,
+            "LoadBalancerDns",
+            value=self.alb.load_balancer_dns_name,
+            export_name=f"{construct_id}-dns",
+        )
